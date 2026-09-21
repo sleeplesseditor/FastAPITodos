72567b1529e6acba47913b9235e654a5d64e182f
sleeplesseditor <josh@bryson.me.uk>
2026-09-21 13:01:45 +0100

    Get all endpoint added

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL =  f"sqlite:///{os.path.join(BASE_DIR, 'todosapp.db')}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- models.py
+++ models.py
@@ -3,10 +3,11 @@ from sqlalchemy import Column, Integer, String, Boolean
 
 class Todos(Base):
     __tablename__ = "todos"
-    id = Column(Integer, primary_key=True)
+
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
-    completed = Column(Boolean, default=False)
+    complete = Column(Boolean, default=False)
 
 
