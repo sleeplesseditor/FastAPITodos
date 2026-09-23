670d104e505c873707d134a41b285aa44f9fdef5
sleeplesseditor <josh@bryson.me.uk>
2026-09-23 09:39:36 +0100

    CreateUser endpoint added

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,29 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
 
 router = APIRouter()
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@router.get("/auth/")
-async def get_user():
-    return {"user": "authenticated"}
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+@router.post("/auth/")
+async def create_user(create_user_req: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_req.email,
+        username=create_user_req.username,
+        first_name=create_user_req.first_name,
+        last_name=create_user_req.last_name,
+        role=create_user_req.role,
+        hashed_password=bcrypt_context.hash(create_user_req.password),
+        is_active=True,
+    )
+
+    return create_user_model
